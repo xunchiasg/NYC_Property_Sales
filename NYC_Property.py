7ff19e45fdbef751423b7754b05308cb9d378159
xunchiasg <xuncjv@gmail.com>
2025-04-10 07:42:19 +0800

    Initial py file

---
+++ NYC_Property.py
@@ -0,0 +1,40 @@
+
+# Library imports
+import streamlit as st
+import os
+
+
+# Title of st app
+st.title("NYC Property Data Analysis")
+st.write("Have you ever wondered how many properties are sold in NYC each year? Or the most popular neighborhoods amongst buyers?")
+st.write("This app, which contains an analysis of NYC property data from 2023 and 2024 will help give you insights into the NYC property market.")
+
+st.header("About the Dataset")
+st.write("ABC")
+
+# Metrics Section
+st.subheader("Key Metrics")
+
+st.header("Transaction Trends")
+st.subheader("By Borough")
+st.write("ABC")
+
+st.subheader("By Month")
+st.write("ABC")
+
+st.subheader("Top 10 Neighborhoods")
+st.write("ABC")
+
+st.subheader("Most Popular Property Types")
+st.write("ABC")
+
+
+
+
+
+
+
+
+
+
+
